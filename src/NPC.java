c44e2e6a4454d7c507743c4cffe8a74f07299b22
Diamantis Spyros (diamaspy) <diamaspy@students.zhaw.ch>
2025-12-20 20:40:52 +0100

    Refactor NPC class to include name and description attributes with corresponding getters.

--- src/NPC.java
+++ src/NPC.java
@@ -1,3 +1,20 @@
 public class NPC {
 
+    private String name;
+    private String description;
+
+
+    public NPC(String name, String description, String itemName, String itemDesc) {
+        this.description = description;
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
