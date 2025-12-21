a6382a94e085b8a149dc26037037781e1905ef03
Diamantis Spyros (diamaspy) <diamaspy@students.zhaw.ch>
2025-12-21 21:10:37 +0100

    Add unit tests for Game class to validate room navigation functionality

--- test/GameTest.java
+++ test/GameTest.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.Assert.assertEquals;
 
 public class GameTest {
+
+    private Game game;
+    private Room square, tavern, forge;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    void testGoEastFromSquareToTavern() {
+        game.processCommand(new Command(CommandWord.GO, "east"));
+        assertEquals("You are in the Black Goat Tavern", game.getCurrentRoom().getShortDescription());
+    }
+
 }
