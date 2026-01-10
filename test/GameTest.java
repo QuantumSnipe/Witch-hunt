dc047ae494105f9d17355111415ba08ea57b2a77
Diamantis Spyros (diamaspy) <diamaspy@students.zhaw.ch>
2026-01-10 16:50:21 +0100

    Added Player instance in Game class

--- test/GameTest.java
+++ test/GameTest.java
@@ -1,16 +1,20 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
+
 import static org.junit.Assert.assertEquals;
 
 public class GameTest {
 
     private Game game;
     private Room square, tavern, forge;
+    private Player player;
+
 
     @BeforeEach
     void setUp() {
         game = new Game();
+        player = new Player();
     }
 
     @Test
@@ -18,5 +22,6 @@ public class GameTest {
         game.processCommand(new Command(CommandWord.GO, "east"));
         assertEquals("You are in the Black Goat Tavern", game.getCurrentRoom().getShortDescription());
     }
+
 
 }
