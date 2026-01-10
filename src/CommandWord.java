976fdcd52d29047e143edcda3995a6a981b1f3db
Diamantis Spyros (diamaspy) <diamaspy@students.zhaw.ch>
2026-01-10 16:19:31 +0100

    feat(game): add 'add' command to CommandWord enum
    - Add "ADD" command to the enum values
    - Make commandString field final for immutability

--- src/CommandWord.java
+++ src/CommandWord.java
@@ -1,11 +1,11 @@
 public enum CommandWord {
 
 	GO("go"), QUIT("quit"), HELP("help"), 
-	TAKE("take"), ASK("ask"), LOOK("look"), 
+	TAKE("take"), ADD("add"), ASK("ask"), LOOK("look"),
 	DROP("drop"), INVENTORY("inventory"), UNLOCK("unlock"), 
 	TALK("talk"), EXAMINE("examine"), UNKNOWN("?");
 
-	private String commandString;
+	private final String commandString;
 
     CommandWord(String commandString) {
 		this.commandString = commandString;
