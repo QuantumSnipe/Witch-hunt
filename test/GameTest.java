5fe699a11b3d36f224657bd1612cb946e60d9eaf
Diamantis Spyros (diamaspy) <diamaspy@students.zhaw.ch>
2026-01-14 14:15:28 +0100

    refactor(test): update GameTest class for method visibility and organization

--- test/GameTest.java
+++ test/GameTest.java
@@ -1,25 +1,26 @@
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-
-
 import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class GameTest {
 
     private Game game;
-    private Room square, tavern, forge;
-    private Player player;
 
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         game = new Game();
-        player = new Player();
     }
 
     @Test
-    void testGoEastFromSquareToTavern() {
+    public void testGoEastFromSquareToTavern() {
         game.processCommand(new Command(CommandWord.GO, "east"));
         assertEquals("You are in the Black Goat Tavern", game.getCurrentRoom().getShortDescription());
+    }
+
+    @Test
+    public void testTakeItem_NoSecondWord() {
+        Command command = new Command(null, null);
+        game.takeItem(command);
     }
 }
