bc7ad3da05d6093d4c2c8ea22559ed8ffdf6f861
Diamantis Spyros (diamaspy) <diamaspy@students.zhaw.ch>
2026-01-21 00:34:49 +0100

    feat(npc): add spell power to Witch class
    Extend Witch class to inherit from NPC and introduce spellPower attribute with getter and setter methods. This enhances the Witch NPC with magical capabilities.

--- src/Witch.java
+++ src/Witch.java
@@ -1,4 +1,19 @@
-public class Witch {
-	
+public class Witch extends NPC {
+
+    private int spellPower;
+
+    public Witch(String name, String description, int spellPower) {
+        super(name, description);
+        this.spellPower = spellPower;
+    }
+
+    public int getSpellPower() {
+        return spellPower;
+    }
+
+    public void setSpellPower(int spellPower) {
+        this.spellPower = spellPower;
+    }
+
 
 }
