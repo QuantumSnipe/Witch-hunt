32f2a4f00a7a5891f77dc811c4959c17d1e7f29a
Diamantis Spyros (diamaspy) <diamaspy@students.zhaw.ch>
2026-01-26 13:27:47 +0100

    (feat.Time) Updated Time class, to include starting and ending time of the game

--- src/Time.java
+++ src/Time.java
@@ -2,7 +2,7 @@ public class Time {
 
     private int time;
     private int elapsedTime;
-    private int endTime;
+    private final int endTime;
 
     public Time(int time, int endHour) {
         this.time = time;
@@ -10,8 +10,37 @@ public class Time {
         this.endTime = endHour;
     }
 
-    public void addMinute(int time) {
-        this.time += time;
-        elapsedTime += time;
+    public void addMinute(int minutes) {
+        this.time += minutes;
+        elapsedTime += minutes;
+    }
+
+    public int getCurrentTime() {
+        return time;
+    }
+
+    public int getElapsedTime() {
+        return elapsedTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public boolean isTimeUp() {
+        int currentHour = (time / 60) % 24;
+        int endHour = endTime % 24;
+
+        if (endHour < 22) {
+            return currentHour >= endHour && currentHour < 22;
+        }
+        return false;
+    }
+
+    public String getFormattedTime() {
+        int totalMinutes = time % 1440;
+        int hours = (totalMinutes / 60) % 24;
+        int minutes = totalMinutes % 60;
+        return String.format("%02d:%02d", hours, minutes);
     }
 }
